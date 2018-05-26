fb383a93ae9d0a503992e90d9fbb995ab7314072
learnfrontend-dc <learnfrontenddc@gmail.com>
2018-05-26 08:48:56 -0500

    script change

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/product-header'));
+app.use(express.static(__dirname + '/dist/productmgt'));
 
 app.get('/*', function(req,res) {
     console.log('__dirname:'+__dirname);
-res.sendFile(path.join(__dirname+'/dist/product-header/index.html'));
+res.sendFile(path.join(__dirname+'/dist/productmgt/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
