b99fc1bf7155772b33681b3879baf21606c1cccf
learnfrontend-dc <learnfrontenddc@gmail.com>
2018-07-11 22:45:12 -0500

    fix for IE poyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -77,3 +77,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import '@webcomponents/custom-elements/custom-elements.min';
